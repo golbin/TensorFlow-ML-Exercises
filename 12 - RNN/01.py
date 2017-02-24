2478806e0e009d7040c8d1327ce3fcd1edb8d300
Sung Kim <hunkim@gmail.com>
2017-02-24 12:40:01 +0800

    Added batch mode

--- 12 - RNN/01.py
+++ 12 - RNN/01.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 import numpy as np
 
-sample = " if you want you"
+sample = " if you want y"
 
 char_set = list(set(sample))  # id -> char
 char_dic = {w: i for i, w in enumerate(char_set)}
@@ -12,16 +12,22 @@ rnn_hidden_size = dic_size = len(char_dic)  # output size of each cell
 batch_size = 1  # one sample data,one batch
 sequence_length = len(sample) - 1  # number of lstm rollings (unit #)
 
+x_data = []
+y_data = []
 sample_idx = [char_dic[c] for c in sample]  # char to index
-x_data = tf.one_hot(sample_idx[:-1], dic_size)  # one hot
-y_data = sample_idx[1:]
+x = tf.one_hot(sample_idx[:-1], dic_size)  # one hot
+y = sample_idx[1:]
+
+x_data.append(x)
+y_data.append(y)
 
 # Make lstm with rnn_hidden_size (each unit input vector size)
 lstm = rnn.BasicLSTMCell(rnn_hidden_size, state_is_tuple=True)
 lstm = rnn.MultiRNNCell([lstm] * 1, state_is_tuple=True)
 
 # split to input (char)length. This will decide unrolling size
-x_split = tf.split(value=x_data, num_or_size_splits=[sequence_length])
+x_data = tf.reshape(x_data, [-1, rnn_hidden_size])
+x_split = tf.split(value=x_data, num_or_size_splits=batch_size)
 
 # outputs: unrolling size x hidden size, state = hidden size
 outputs, _states = rnn.static_rnn(lstm, x_split, dtype=tf.float32)
@@ -31,7 +37,7 @@ softmax_w = tf.get_variable("softmax_w", [sequence_length, dic_size])
 softmax_b = tf.get_variable("softmax_b", [dic_size])
 outputs = outputs * softmax_w + softmax_b
 
-outputs = tf.reshape(outputs, [batch_size, sequence_length, dic_size])
+outputs = tf.reshape(outputs, [-1, sequence_length, dic_size])
 y_data = tf.reshape(y_data, [batch_size, sequence_length])
 weights = tf.ones([batch_size, sequence_length])
 
