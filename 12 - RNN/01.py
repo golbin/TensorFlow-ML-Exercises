48b342ad8fff931f553458fe688b5ca583bb5242
Sung Kim <hunkim@gmail.com>
2017-02-24 13:09:04 +0800

    simplication

--- 12 - RNN/01.py
+++ 12 - RNN/01.py
@@ -12,14 +12,9 @@ rnn_hidden_size = dic_size = len(char_dic)  # output size of each cell
 batch_size = 1  # one sample data,one batch
 sequence_length = len(sample) - 1  # number of lstm rollings (unit #)
 
-x_data = []
-y_data = []
 sample_idx = [char_dic[c] for c in sample]  # char to index
-x = tf.one_hot(sample_idx[:-1], dic_size)  # one hot
-y = sample_idx[1:]
-
-x_data.append(x)
-y_data.append(y)
+x_data = tf.one_hot(sample_idx[:-1], dic_size)  # one hot
+y_data = sample_idx[1:]
 
 # Make lstm with rnn_hidden_size (each unit input vector size)
 lstm = rnn.BasicLSTMCell(rnn_hidden_size, state_is_tuple=True)
@@ -37,16 +32,18 @@ softmax_w = tf.get_variable("softmax_w", [sequence_length, dic_size])
 softmax_b = tf.get_variable("softmax_b", [dic_size])
 outputs = outputs * softmax_w + softmax_b
 
-outputs = tf.reshape(outputs, [-1, sequence_length, dic_size])
+outputs = tf.reshape(outputs, [batch_size, sequence_length, dic_size])
 y_data = tf.reshape(y_data, [batch_size, sequence_length])
 weights = tf.ones([batch_size, sequence_length])
 
 sequence_loss = tf.contrib.seq2seq.sequence_loss(outputs, y_data, weights)
-mean_loss = tf.reduce_mean(sequence_loss) / batch_size
+mean_loss = tf.reduce_mean(sequence_loss)
 train_op = tf.train.AdamOptimizer(learning_rate=0.1).minimize(mean_loss)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
+
+print(sess.run(x_split))
 
 for i in range(1000):
     _, l = sess.run([train_op, mean_loss])
